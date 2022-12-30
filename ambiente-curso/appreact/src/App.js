ee18df7e5e8c65188dd2eae9ff12eea9d71d58bc
Lucas Costa <luchtml5@gmail.com>
2022-12-30 16:05:20 -0300

    mostrar na tela os produtos (erro)

--- ambiente-curso/appreact/src/App.js
+++ ambiente-curso/appreact/src/App.js
@@ -1,54 +1,41 @@
 import React from 'react';
 
-
-//Desafio do curso de React
-//mostrar na tela nome, idade, situação (se ativa, verde, senão, vermelho) e total gasto
-//(caso maior que 10000 mostrar a frase 'gastando muito')
-const lucas = {
-  cliente: 'Lucas',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
-const thamires = {
-  cliente: 'Thamires',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  const dados = lucas;
-  const total = dados.compras
-    .map((compra) => {
-      return Number(compra.preco.replace('R$ ', ''));
-    })
-    .reduce((acc, preco) => {
-      return acc + preco;
-    });
+
+  const filtroProdutos = produtos.filter(({preco}) => {
+    return Number(preco.replace("R$ ", "")) > 1500
+  })
 
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total gasto: R$: {total}</p>
-      {total > 10000 && <p>Você está gastando muito</p>}
-    </div>
+    <section>
+      {filtroProdutos.map((({ id, nome, preco, cores }) => {
+        <div key={id}>
+          <h1>{ nome }</h1>
+          <p>Preço: { preco }</p>
+        </div>
+      }))}
+    </section>
   );
 };
 
