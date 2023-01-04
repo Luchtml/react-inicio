4a1c50f9ca204b42e6832ebd5eae506fa3f1d8e9
Lucas Costa <luchtml5@gmail.com>
2023-01-04 18:34:10 -0300

    props com rest e spread, e tipos de dados para props

--- ambiente-curso/appreact/src/App.js
+++ ambiente-curso/appreact/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-
-const Titulo = (props) => {
-  return <h1 style={{color: props.cor}}>{props.texto}</h1>
-}
+import Form from './Form/Form';
 
 const App = () => {
 
   return <div>
-    <Titulo cor="red" texto="Meu Titulo 1"/>
-    <Titulo cor="blue" texto="Meu Titulo 2"/>
-    <Titulo texto="Meu Titulo 3"/>
+    <Form/>
   </div>
 };
 
