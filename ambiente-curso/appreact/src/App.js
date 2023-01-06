0b7cf26eaea6888e85f40ad5de915703db0420a0
Lucas Costa <luchtml5@gmail.com>
2023-01-06 18:29:44 -0300

    reatividade

--- ambiente-curso/appreact/src/App.js
+++ ambiente-curso/appreact/src/App.js
@@ -4,9 +4,16 @@ import Modal from './Modal';
 
 const App = () => {
   const [modal, setModal] = React.useState(false)
+  const [items, setItems] = React.useState('Teste')
+
+  function handleClick() {
+    setItems('Outro')
+  }
 
   return (
     <div>
+      <p>{items}</p>
+      <button onClick={handleClick}>Clicar</button>
       <Modal modal={modal} setModal={setModal} />
      <ButtonModal setModal={setModal}/>
     </div>
