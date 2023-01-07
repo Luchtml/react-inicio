c9811df71036e900135c204ee7043e8cda7d0ae8
Lucas Costa <luchtml5@gmail.com>
2023-01-07 13:31:18 -0300

    botao de produtos e fetch dos dados

--- ambiente-curso/appreact/src/App.js
+++ ambiente-curso/appreact/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import Produto from './Produto';
+ /* 
+ https://ranekapi.origamid.dev/json/api/produto/tablet
+ https://ranekapi.origamid.dev/json/api/produto/smartphone
+ https://ranekapi.origamid.dev/json/api/produto/notebook
+Crie um,a interface com 3 botões, um para cada produto
+Ao clicar no botão faça um fetch a api e mostre os dados dos produtos
+mostre apenas um produto por vez
+mostre a mensagem carregando enquanto o fetch é realizado
+*/
 
 const App = () => {
-  const [contar, setContar] = React.useState(1);
-  const [items, setItems] = React.useState(['Item 1']);
+  const [dados, setDados] = React.useState(null)
 
-  function handleClick() {
-    setContar(contar + 1);
-    setItems([...items, 'Item' + (contar + 1)]);
+  async function handleClick(event) {
+    const response = await (await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`))
+    const json = await response.json()
+    setDados(json)
   }
 
   return (
     <div>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>{contar}</button>
+      <button onClick={handleClick} style={{margin: ".5rem"}}>notebook</button>
+      <button onClick={handleClick} style={{margin: ".5rem"}}>smartphone</button>
+      <button onClick={handleClick} style={{margin: ".5rem"}}>tablet</button>
+      <Produto dados={dados} />
     </div>
   );
 };
---
+++ ambiente-curso/appreact/src/Produto.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Produto = ({dados}) => {
+
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Produto
