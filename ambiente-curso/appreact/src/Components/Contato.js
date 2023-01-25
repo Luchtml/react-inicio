fa92d79ac9ddb65e1f49423158fc8351c1ef0751
Lucas Costa <luchtml5@gmail.com>
2023-01-25 10:46:31 -0300

    exercicio3

--- ambiente-curso/appreact/src/Components/Contato.js
+++ ambiente-curso/appreact/src/Components/Contato.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import styles from './Contato.module.css';
+import React from "react";
+import styles from "./Contato.module.css";
+import foto from "../img/contato.jpg";
+import Head from "./Head";
 
 const Contato = () => {
   return (
-    <div>
-      <h1>Contato</h1>
-    </div>
+    <section className={`${styles.contato} animeLeft`}>
+      <Head title="Ranke | Contato" description="Entre em contato" />
+      <img src={foto} alt="Máquina de escrever" />
+      <div>
+        <h1>Entre em cotato.</h1>
+        <ul className={styles.dados}>
+          <li>email@contato.com</li>
+          <li>98558-4512</li>
+          <li>Rua ali perto</li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
